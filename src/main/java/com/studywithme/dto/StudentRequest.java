48aabb28cd09259648ed447b1e0dee60b4389de0
Nishant Raj <nishant.read@gmail.com>
2026-08-15 19:57:01 +0530

    Saturday, August 15, 2026 7:56:59 PM

--- src/main/java/com/studywithme/dto/StudentRequest.java
+++ src/main/java/com/studywithme/dto/StudentRequest.java
@@ -2,11 +2,15 @@ package com.studywithme.dto;
 
 import lombok.*;
 
+// For DTOs, many developers prefer
+// because it is explicit and avoids generating unnecessary methods like equals(), hashCode(), and toString() when they are not needed.
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
+// We Can use @Data as well. Yes We can , But we prefer that one
 public class StudentRequest {
 
     private String name;
