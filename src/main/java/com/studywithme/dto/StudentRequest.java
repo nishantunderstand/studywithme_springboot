bc3d252298fe72bb438d5a119bea5da885ef07b3
Nishant Raj <nishant.read@gmail.com>
2026-08-16 22:21:46 +0530

    Sunday, August 16, 2026 10:21:45 PM

--- src/main/java/com/studywithme/dto/StudentRequest.java
+++ src/main/java/com/studywithme/dto/StudentRequest.java
@@ -12,14 +12,9 @@ import lombok.*;
 
 // We Can use @Data as well. Yes We can , But we prefer that one
 public class StudentRequest {
-
     private String name;
-
     private String email;
-
     private Integer age;
-
     private String course;
-
     private Long departmentId;
 }
