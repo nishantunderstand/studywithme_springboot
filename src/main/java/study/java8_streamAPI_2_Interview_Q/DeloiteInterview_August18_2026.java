df411f0160143f57fa1a2c493782c8226a4165d3
Nishant Raj <nishant.read@gmail.com>
2026-09-12 02:23:13 +0530

    Saturday, September 12, 2026 2:23:11 AM

--- src/main/java/study/java8_streamAPI_2_Interview_Q/DeloiteInterview_August18_2026.java
+++ src/main/java/study/java8_streamAPI_2_Interview_Q/DeloiteInterview_August18_2026.java
@@ -1,13 +1,30 @@
 package study.java8_streamAPI_2_Interview_Q;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Created : 2026-08-09 12:25:47
  */
 
-
+/**
+ * Merge Two Integer Array
+ * Merge Two Integer Array Then Sort it
+ * Merge Two Integer Array Then Sort it ASC and DESC
+ * <p>
+ * What if Array is Primitive Datatype
+ */
 public class DeloiteInterview_August18_2026 {
     public static void main(String[] args) {
-        Integer[] ar1 ={1,2,3};
-        Integer[] ar2 ={4,5,6};
+        Integer[] ar1 = {1, 4, 2, 3, 9};
+        Integer[] ar1a = {1, 2, 3};
+        Integer[] ar2 = {11, 4, 33, 5, 6};
+        Integer[] ar2a = {4, 5, 6};
+        int[] arr1 = {99, 11, 22, 33, 55, 900, 1, 2, 24};
+        int[] arr2 = {9, 1, 2, 3, 5, 0, 111, -12, 24};
+
     }
+
 }
--- src/main/java/study/java8_streamAPI_2_Interview_QA/DeloiteInterview_August18_2026.java
+++ src/main/java/study/java8_streamAPI_2_Interview_QA/DeloiteInterview_August18_2026.java
@@ -1,18 +1,55 @@
 package study.java8_streamAPI_2_Interview_QA;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
  * Created : 2026-08-09 12:25:47
  */
 
-
+/**
+ * Merge Two Integer Array
+ * Merge Two Integer Array Then Sort it
+ * Merge Two Integer Array Then Sort it ASC and DESC
+ *
+ * What if Array is Primitive Datatype
+ */
 public class DeloiteInterview_August18_2026 {
     public static void main(String[] args) {
-        Integer[] ar1 ={1,2,3};
-        Integer[] ar2 ={4,5,6};
-        Integer[] result = Stream.concat(Arrays.stream(ar1),Arrays.stream(ar2)).toArray(Integer[]::new);
+        Integer[] ar1 ={1,4, 2,3,9 };
+        Integer[] ar1a ={1,2,3};
+        Integer[] ar2 ={11,4,33, 5,6};
+        Integer[] ar2a ={4,5,6};
+
+
+
+        Integer[] result1 = Stream.concat(Arrays.stream(ar1), Arrays.stream(ar2)).toArray(Integer[]::new);
+
+        Integer[] result2 = Stream.concat(Arrays.stream(ar1), Arrays.stream(ar2))
+                .sorted()
+                .toArray(Integer[]::new);
+        Integer[] result3 = Stream.concat(Arrays.stream(ar1), Arrays.stream(ar2))
+                .sorted(Comparator.comparing(Integer::intValue).reversed())
+                .toArray(Integer[]::new);
+
+
+        int[] arr1 = {99, 11,22,33,55,900,1,2,24};
+        int[] arr2 = {9, 1,2,3,5,0,111,-12,24};
+
+        int[] result4 = IntStream.concat(
+                        Arrays.stream(arr1),
+                        Arrays.stream(arr2)
+                ).sorted()
+                .toArray();
+
+        Integer[] result = Stream.concat(Arrays.stream(arr1).boxed(), Arrays.stream(arr2).boxed())
+                .sorted()
+                .toArray(Integer[]::new);
+
+
         System.out.println(Arrays.toString(result));
     }
+
 }
